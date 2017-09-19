c091705216a085a0b66a874e610d686252144db1
Christopher McArthur <prince.chrismc@gmail.com>
2017-09-18 20:10:23 -0400

    colored panes with no window

--- TestJava-ServerGUI/src/ApplicationController.java
+++ TestJava-ServerGUI/src/ApplicationController.java
@@ -8,7 +8,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -19,9 +21,9 @@ public class ApplicationController extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
-        
+        stage.initStyle(StageStyle.TRANSPARENT);
         Scene scene = new Scene(root);
-        
+        scene.setFill(Color.TRANSPARENT);
         stage.setScene(scene);
         stage.show();
     }
